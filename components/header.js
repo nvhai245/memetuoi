910ad19eb76c9894ccc0df1b935f2a38e25b193d
Hari <nvhai245@gmail.com>
2019-07-30 05:30:49 +0700

    moved workspace to Windows 10, nothing changes

---
+++ components/header.js
@@ -0,0 +1,7 @@
+const header = () => {
+    return ( 
+        <h1>fuckyou</h1>
+     );
+}
+ 
+export default header;
--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ const mongooseOptions = {
   useFindAndModify: false
 };
 
-mongoose.connect(process.env.MONGO_URL, mongooseOptions).then(() => console.log('DB connected'));
+mongoose.connect(process.env.MONGO_URI, mongooseOptions).then(() => console.log('DB connected'));
 mongoose.connection.on("error", err => {
   console.log(`DB connection error: ${err.message}`);
 });
