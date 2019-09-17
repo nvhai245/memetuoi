6c15ccd272b33d40e5e0ed03962c9704f6e8755d
Hari <nvhai245@gmail.com>
2019-09-17 23:43:56 +0700

    minor fixes

--- server/passport.js
+++ server/passport.js
@@ -10,14 +10,14 @@ passport.use(User.createStrategy());
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/auth/facebook/callback"
+  callbackURL: "https://fresh-memes.herokuapp.com/auth/facebook/callback"
 },
   authController.fbLogin
 ));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://fresh-memes.herokuapp.com/auth/google/callback"
 },
 authController.ggLogin
 ));
